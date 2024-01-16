59a22520da02f668283d0eda3ce324c2d039ff76
xKotelek <contact@kotelek.dev>
2024-01-16 20:44:48 +0100

    Create update.ps1

---
+++ dist/update.ps1
@@ -0,0 +1,14 @@
+# Get drive with System32 folder
+$systemDrive = (Get-WmiObject Win32_OperatingSystem).SystemDrive
+
+# Wait for 1 second until easy close
+Start-Sleep -Seconds 1
+
+# Download easy file
+Invoke-WebRequest -Uri "https://github.com/easy-pkg/easy/raw/main/dist/easy.exe" -OutFile "$systemDrive\Windows\System32\easy.exe"
+
+# Wait for another 1 second
+Start-Sleep -Seconds 1
+
+# Download default easy config
+Invoke-WebRequest -Uri "https://github.com/easy-pkg/easy/raw/main/dist/easy.config.json" -OutFile "$systemDrive\Windows\System32\easy.config.json"

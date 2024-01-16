1bd9ad705a89e888545bc700b1a83ef8cd41a34a
xKotelek <contact@kotelek.dev>
2024-01-16 20:51:27 +0100

    Update update.ps1

--- dist/update.ps1
+++ dist/update.ps1
@@ -1,14 +1,12 @@
 # Get drive with System32 folder
 $systemDrive = (Get-WmiObject Win32_OperatingSystem).SystemDrive
-
-# Wait for 1 second until easy close
+# Wait for 1 second until easy closed
 Start-Sleep -Seconds 1
-
 # Download easy file
 Invoke-WebRequest -Uri "https://github.com/easy-pkg/easy/raw/main/dist/easy.exe" -OutFile "$systemDrive\Windows\System32\easy.exe"
-
 # Wait for another 1 second
 Start-Sleep -Seconds 1
-
 # Download default easy config
 Invoke-WebRequest -Uri "https://github.com/easy-pkg/easy/raw/main/dist/easy.config.json" -OutFile "$systemDrive\Windows\System32\easy.config.json"
+# Output the message
+Write-Output "$(Write-Host -ForegroundColor Green '✔') $(Write-Host -ForegroundColor Magenta 'easy') $(Write-Host -ForegroundColor White 'will be updated after this message.')"
